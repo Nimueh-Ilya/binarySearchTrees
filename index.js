8544364c6b4e2263533d2ada519c5ed0e9b20fe4
Nimueh <139075582+Nimueh-Ilya@users.noreply.github.com>
2024-02-06 16:56:35 +0100

    Working node tree

--- index.js
+++ index.js
@@ -1,7 +1,43 @@
 class Node {
-  constructor(value) {
-    this.value = value;
-    this.leftNode = null;
-    this.rightNode = null;
+  constructor(data) {
+    this.data = data;
+    this.left = null;
+    this.right = null;
   }
 }
+class Tree {
+  constructor(array) {
+    this.array = array;
+  }
+  cleanArray() {
+    return [...new Set(this.array.sort((a, b) => a - b))];
+  }
+
+  buildTree(arr, start, end = arr.length) {
+    if (start > end) {
+      return null;
+    }
+    let mid = parseInt((start + end) / 2);
+    let node = new Node(arr[mid]);
+    node.left = this.buildTree(arr, start, mid - 1);
+    node.right = this.buildTree(arr, mid + 1, end);
+    return node;
+  }
+}
+const prettyPrint = (node, prefix = "", isLeft = true) => {
+  if (node === null) {
+    return;
+  }
+  if (node.right !== null) {
+    prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+  }
+  console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+  if (node.left !== null) {
+    prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+  }
+};
+const arr = [1, 2, 3, 56, 56, 4, 5, 6, 7, 8, 9];
+const newTree = new Tree(arr);
+const myNode = newTree.buildTree(newTree.cleanArray(), 0);
+console.log(myNode);
+prettyPrint(myNode);
